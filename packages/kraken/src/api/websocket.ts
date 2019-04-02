90d758e0c4ad15a50c63c0b5e556e8a8f5b2bb03
Sebastian Siemssen <fubhy@fubhy.com>
2019-04-03 00:33:37 +0200

    Proper types for websocket api integration.

--- packages/kraken/src/api/websocket.ts
+++ packages/kraken/src/api/websocket.ts
@@ -1,19 +1,122 @@
 import * as Rx from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap, filter } from 'rxjs/operators';
 import isomorphicWs from 'isomorphic-ws';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import * as debug from '../debug';
+import { SellOrBuyAbbreviated, MarketOrLimitAbbreviated } from './types';
 
-const url = 'wss://ws.kraken.com';
+export interface Event {
+  event: string;
+}
 
-let socket$: WebSocketSubject<any>;
+export interface PingMessage extends Event {
+  event: 'ping';
+  reqid?: number;
+}
+
+export interface PongMessage extends Event {
+  event: 'pong';
+  reqid?: number;
+}
+
+export interface HeartbeatMessage extends Event {
+  event: 'heartbeat';
+}
+
+export interface SystemStatusMessage extends Event {
+  connectionID: string;
+  event: 'systemStatus';
+  status: 'online' | 'maintenance' | string;
+  version: string;
+}
+
+export interface SubscribeMessage extends Event {
+  event: 'subscribe';
+  pair: string[];
+  subscription: SubscriptionParams;
+  reqid?: number;
+}
+
+export interface UnsubscribeMessage extends SubscribeMessage {
+  event: 'subscribe';
+}
+
+export interface SubscriptionStatusMessage extends Event {
+  channelID: number;
+  event: 'subscriptionStatus';
+  status: 'subscribed' | 'unsubscribed' | 'error';
+  pair: string;
+  reqid: number;
+  subscription: SubscriptionParams;
+  errorMessage?: string;
+}
+
+export interface TickerMessage {
+  a: [string, number, string];
+  b: [string, number, string];
+  c: [string, string];
+  v: [string, string];
+  p: [string, string];
+  t: [number, number];
+  l: [string, string];
+  h: [string, string];
+  o: [string, string];
+}
+
+export type OhlcMessage = [
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  number
+];
+
+export type TradeMessage = [
+  string,
+  string,
+  string,
+  SellOrBuyAbbreviated,
+  MarketOrLimitAbbreviated,
+  string
+][];
+
+export type SpreadMessage = [string, string, string];
+
+export interface BookMessage {
+  as: [string, string, string];
+  bs: [string, string, string];
+}
+
+export type SubscriptionMessage =
+  | OhlcMessage
+  | TradeMessage
+  | SpreadMessage
+  | BookMessage;
+
+export type RawSubscriptionMessage = [number, SubscriptionMessage];
+
+export type AnyMessage =
+  | PingMessage
+  | PongMessage
+  | HeartbeatMessage
+  | SystemStatusMessage
+  | SubscribeMessage
+  | UnsubscribeMessage
+  | SubscriptionStatusMessage
+  | RawSubscriptionMessage;
+
+let socket$: WebSocketSubject<AnyMessage>;
 export const connect = () => {
   if (!socket$) {
     const open$ = new Rx.Subject();
     const close$ = new Rx.Subject();
 
     socket$ = webSocket({
-      url,
+      url: 'wss://ws.kraken.com',
       WebSocketCtor: isomorphicWs,
       closeObserver: close$,
       openObserver: open$,
@@ -38,30 +141,33 @@ export interface SubscriptionParams {
 }
 
 let counter = 0;
-export const subscribe = (pair: string[], subscription: SubscriptionParams) => {
-  let channels = [];
+export const subscribe = <T = SubscriptionMessage>(
+  pair: string[],
+  subscription: SubscriptionParams,
+): Rx.Observable<T> => {
+  let channels: string[] = [];
   const reqid = ++counter;
 
-  const sub = () => ({
+  const subMsg = () => ({
     reqid,
     pair,
     subscription,
     event: 'subscribe',
   });
 
-  const unsub = () => ({
+  const unsubMsg = () => ({
     reqid,
     pair,
     subscription,
     event: 'unsubscribe',
   });
 
-  const filter = message => {
-    const req = message.reqid && message.reqid === reqid;
-    if (message.event === 'subscriptionStatus' && req) {
-      if (message.status === 'subscribed') {
+  const filterFn = (message: any) => {
+    if (message.event === 'subscriptionStatus') {
+      const req = message.reqid && message.reqid === reqid;
+      if (req && message.status === 'subscribed') {
         channels = [...channels, message.channelID];
-      } else if (message.status === 'unsubscribed') {
+      } else if (req && message.status === 'unsubscribed') {
         channels = channels.filter(channel => channel !== message.channelID);
       }
 
@@ -71,18 +177,14 @@ export const subscribe = (pair: string[], subscription: SubscriptionParams) => {
     return Array.isArray(message) && channels.indexOf(message[0]) !== -1;
   };
 
-  const multiplex$ = connect().multiplex(sub, unsub, filter);
+  const multiplex$ = connect().multiplex(subMsg, unsubMsg, filterFn);
   return multiplex$.pipe(
-    map(value => {
-      if (Array.isArray(value)) {
-        return value[1];
-      }
-
+    tap(value => {
       if (value.status && value.status === 'error') {
         throw new Error(value.errorMessage);
       }
-
-      return value;
     }),
+    filter(value => Array.isArray(value)),
+    map(value => value[1]),
   );
 };
